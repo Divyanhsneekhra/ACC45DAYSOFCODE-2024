4e0ab0ba1572ba46194ddaf70aef472d4a681a75
DIVYANSH NEEKHRA <divyanshneekhra2006@gmail.com>
2024-10-04 10:43:50 +0530

    Day5.c

---
+++ Day5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+ // First number
+ int a = 5;
+ // Second number
+ int b = 3;
+ // Add the numbers
+ int sum = a + b;
+ // Print the result
+ printf("Sum: %d\n", sum);
+ return 0;
+}
