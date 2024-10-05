ea434927483f78c0ad3f9a7e754d349f6c66254e
DIVYANSH NEEKHRA <divyanshneekhra2006@gmail.com>
2024-10-05 22:00:03 +0530

    Day6.c

---
+++ Day6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+ 
+ // Input values
+ float P = 1000, R = 20, T = 10;
+ // Calculate simple interest
+ float SI = (P * T * R) / 100;
+ // Print Simple Interest
+ printf("Simple Interest = %f\n", SI);
+ return 0;
+}
